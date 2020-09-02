08749ed8919adade93729a8e534fd11e9eaa2c06
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-09-03 05:05:46 +0900

    chapter2追加

---
+++ chapter2/1-1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy.random as random
+import scipy as sp
+import pandas as pd
+from pandas import Series, DataFrame
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+%matplotlib inline
+
+%precision 3
