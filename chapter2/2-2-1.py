c3c9899bed9fab01d720d9af4a036fe702d61111
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-10-03 22:22:17 +0900

    npの処理追加

--- chapter2/2-2-1.py
+++ chapter2/2-2-1.py
@@ -35,3 +35,16 @@ print(random.choice(data,10,replace = False))
 random.seed(1)
 print(random.choice(data,10))
 print(random.choice(data,10,replace = False))
+
+print(np.arange(9))
+data1 = np.arange(9).reshape(3,3)
+print(data1)
+
+data2 = np.arange(9, 18).reshape(3,3)
+print(data2)
+x = np.dot(data1, data2)
+print(x)
+print(data1 * data2)
+
+print(np.zeros((2, 3), dtype = np.int64))
+print(np.ones((2, 3), dtype = np.float64))
