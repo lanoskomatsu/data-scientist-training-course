4b5446ffc90d38988e0a448af59d3a862326d248
lanoskomatsu <yusuke.komatsu@coincheck.com>
2020-10-13 21:23:31 +0900

    scipyの基礎

---
+++ chapter2/2-3.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy.random as random
+import scipy.linalg as linalg
+from scipy.optimize import minimize_scalar
+
+
+matrix = np.array([[1,-1,-1], [-1,1,-1], [-1,-1,1]])
+print(linalg.det(matrix))
